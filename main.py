ad5455e3e7168153c668cefaca5c3b721d283d3a
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2022-11-08 10:41:04 +0100

    Working main
    Added logger to main. Rest of function ready to be transfered to function call from direct call

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# /bin/bashs
+#!/usr/bin/env python
+import logging
+import asyncio
+import wrapper
+
+def main(logger):
+    putanja = wrapper.path()
+    logging.info(putanja)
+
+if __name__ == "__main__":
+    # log_level = "WARNING"
+    log_level = "INFO"
+    logging.basicConfig(level=getattr(logging, log_level))
+    main(log_level)
---
+++ wrapper.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import os
+import datetime
+import logging
+import asyncio
+
+"""
+    ulazni podaci
+"""
+INTEGRACIJSKO_VRIJEME = 10
+BROJ_OCITANJA_ZA_INTERPOLACIJU = 1
+BROJ_MJERENJA = 3
+VREMENSKI_ODMAK = 0
+IZLAZNA_MAPA = ".data/"
+
+
+
+"""
+    kôd
+"""
+def main(logger):
+    putanja = path()
+    print(putanja)
+    os.system(f"demo.py --integration-time-ms={INTEGRACIJSKO_VRIJEME} --scans-to-average={BROJ_OCITANJA_ZA_INTERPOLACIJU} --max={BROJ_MJERENJA} --delay-ms={VREMENSKI_ODMAK} --outfile={putanja} --ascii-art --log-level={logger}")
+
+def path(enum=True, timestamp=True):
+    foo_path = IZLAZNA_MAPA
+
+    if timestamp:
+        foo_path += datetime.datetime.now().strftime('%Y_%m_%d')
+    if enum:
+        counter = 1
+        while os.path.exists(foo_path+"-"+str(counter)+".csv"):
+            counter += 1
+        foo_path += "-" + str(counter)
+    foo_path += ".csv"
+    return foo_path
+
+async def demo(akvizicija,blenda,n,razmak,csv):
+    pass
+
+if __name__ == "__main__":
+    log_level = "WARNING"
+    logging.basicConfig(level=getattr(logging, log_level))
+    main(log_level)
