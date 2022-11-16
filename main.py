a304bdce42035672786f9477883bee3dde47bea2
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2022-11-16 10:14:09 +0100

    Temp + Demo output
    Finall print out. Just need some code optimisation.

--- main.py
+++ main.py
@@ -2,30 +2,30 @@
 import logging
 import asyncio
 import os
-import datetime
+# import datetime
 import platform
 import subprocess
 
 
 from asyncua import Client
-import crystapp_04
+# import crystapp_04
 # from asyncua.crypto.security_policies import SecurityPolicyBasic256Sha256
 
 
-OPC_ADDR = "172.27.39.99"
+# OPC_ADDR = "172.27.39.99"
 INTEGRACIJSKO_VRIJEME = 10
 BROJ_OCITANJA_ZA_INTERPOLACIJU = 1
 BROJ_MJERENJA = 3
 VREMENSKI_ODMAK = 0
-IZLAZNA_MAPA = ".data/"
+# IZLAZNA_MAPA = ".data/"
 
 
-async def wrapper(logger):
+async def wrapper(logger, outfile):
     # call demo_custom.py --help for list of parameters
     if platform.system().lower()=="windows":
         os.system(f"demo_custom.py --integration-time-ms={INTEGRACIJSKO_VRIJEME} \
             --scans-to-average={BROJ_OCITANJA_ZA_INTERPOLACIJU} --max=1 \
-                --delay-ms={VREMENSKI_ODMAK} --outfile='.buffer.csv' \
+                --delay-ms={VREMENSKI_ODMAK} --outfile={outfile} \
                     --ascii-art --log-level={logger}")
     else:
         command = [
@@ -37,7 +37,7 @@ async def wrapper(logger):
             "--max",
             "1",
             "--outfile",
-            ".buffer.csv",
+            str(outfile),
             "--ascii-art",
             "--log-level",
             logging.getLevelName(logger)
@@ -45,44 +45,46 @@ async def wrapper(logger):
         subprocess.call(command)
 
 
-async def temp_read():
-    cert = ".certs/certificate.pem"
-    key = ".certs/key.pem"
-    # NOTE: init
-    client = Client(url=f"opc.tcp://{OPC_ADDR}:4840/freeopcua/server/")
-    # await client.set_security(
-    #     SecurityPolicyBasic256Sha256,
-    #     certificate=".certs/certificate.pem",
-    #     private_key=".certs/key.pem",
-    # )
+async def temp_read(logger, opc_ip):
+    url = "opc.tcp://"+opc_ip+":4840/freeopcua/server/"
+    temp = None
 
-    # NOTE: start
-    # async with client:
-    #     idx = await client.get_namespace_index(
-    #         "urn:freeopcua:python:server"
-    #     )
-    #     logging.info(await client.nodes.objects.call_method(f"{idx}:External_temperature", False))
+    async with Client(url=url) as client:
+        idx = await client.get_namespace_index(
+            "urn:freeopcua:python:server"
+        )
+        folder = await client.nodes.objects.get_child(f"{idx}:Devices")
+        device = await folder.get_child(f"{idx}:JulaboMagio")
+        temp = await device.call_method(f"{idx}:External_temperature", False)
+    
+    return temp
 
 
 def main(logger):
-    broj_mjerenja, vremenski_odmak, izlazna_mapa = 3, 0, ".data/"
-    data_path = crystapp_04.set_filename(folder=IZLAZNA_MAPA)
+    # NOTE: call demo.py
     buffer_path = ".buffer.csv"
+    asyncio.run(wrapper(logger, buffer_path))
+
+    opc_ip = "192.168.0.118"
+    logging.info(asyncio.run(temp_read(logger, opc_ip)))
+
+    # broj_mjerenja, vremenski_odmak, izlazna_mapa = 3, 0, ".data/"
+    # data_path = crystapp_04.set_filename(folder=IZLAZNA_MAPA)
+    
     # jul_1 = julabo.JulaboMS(julabo.connection_for_url("tcp://178.238.237.121:5050"))
-    logging.info(data_path)
-    asyncio.run(wrapper(logger))
+    # logging.info(data_path)
+    
     # logging.info(asyncio.run(get_ms(jul_1, "identification")))
     # logging.info(asyncio.run(get_ms(jul_1, "external_temperature")))
-    for _ in range(broj_mjerenja):
-        pass
+    # for _ in range(broj_mjerenja):
+        # pass
 
     # logging.info(data_path)
-    # logging.info(asyncio.run(temp_read()))
 
     # for _ in range(broj_mjerenja):
     #     pass
-    if not crystapp_04.path_exists(data_path):
-        open(data_path, "x", encoding="UTF-8")
+    # if not crystapp_04.path_exists(data_path):
+        # open(data_path, "x", encoding="UTF-8")
 
             # with open(data_path, "w", encoding="UTF-8") as file:
             #     file.write(table_header)
