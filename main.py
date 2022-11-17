6c6106fdf253678e8687c787c7fcdc475d99d66b
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2022-11-17 09:23:27 +0100

    Class finished
    Writer class finished. Just missing temp to buffer transaction.

--- main.py
+++ main.py
@@ -8,18 +8,53 @@ from crystapp_04 import wrapper, temp_read, path_exists, set_filename
 
 
 class GentleFileWriter:
-    def __init__(self, folder, buffer):
+    def __init__(self, folder, buffer, filename=None):
         self.buffer_path = pathlib.Path(buffer)
-        self.data_path = pathlib.Path(set_filename(folder=folder))
+        if filename is None:
+            self.data_path = pathlib.Path(set_filename(folder=folder))
+        else:
+            self.data_path = pathlib.Path(folder+filename)
+        self.file = None
 
     def exists(self):
         return self.data_path.is_file()
 
+    def _add_header(self):
+        if not self.exists():
+            with open(self.data_path, "w+", encoding="UTF-8") as file:
+                with open(self.buffer_path, "r", encoding="UTF-8") as buffer:
+                    file.write(buffer.readline())
+        else:
+            if not self.header_is_ok():
+                raise BufferError("New data colides old data!")
+
+    def _delete(self):
+        self.data_path.unlink()
+
+    def header_is_ok(self):
+        with open(self.data_path, "r", encoding="UTF-8") as file:
+                with open(self.buffer_path, "r", encoding="UTF-8") as buffer:
+                    if file.readline() == buffer.readline():
+                        return True
+                    return False
+
+    def is_almost_empty(self):
+        with open(self.data_path, "r", encoding="UTF-8") as file:
+            # NOTE: read header first, that is the "almost" empty part
+            file.readline()
+            if not file.readline().strip():
+                return True
+            return False
+
     def __enter__(self):
-        pass
+        self._add_header()
+        self.file = open(self.data_path, "a", encoding="UTF-8")
+        return self.file
 
     def __exit__(self, exc_type, exc_value, traceback):
-        pass
+        self.file.close()
+        if self.is_almost_empty():
+            self._delete()
 
 
 def main(logger):
@@ -34,8 +69,12 @@ def main(logger):
     temp = 100.00
 
     # NOTE: buffer + temp => file
-    file = GentleFileWriter(".data/", buffer_path)
-    logging.info(file.exists())
+    writer = GentleFileWriter(".data/", buffer_path)
+    with writer as file:
+        # file.write("Hello")
+    # logging.info(file.create())
+    # with GentleFileWriter(".data/", buffer_path) as file:
+    #     logging.info(file)
 
     # data_folder = ".data"
     # data_path = set_filename(folder=data_folder)
