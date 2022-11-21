ae8b92d6202900978c0845b8fc825739cc8dcc4d
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2022-11-21 10:53:23 +0100

    bugfix
    should work now -> writing to one file all data.

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ from crystapp_04 import wrapper, temp_read, \
     GentleFileWriter, BROJ_MJERENJA, VREMENSKI_ODMAK
 
 
-def blend_in(buffer_path, temp):
-    with GentleFileWriter(".data/", buffer_path) as file:
-        with open(buffer_path, "r", encoding="UTF-8") as buffer:
+def blend_in(writer, temp):
+    with writer as file:
+        with open(writer.buffer_path, "r", encoding="UTF-8") as buffer:
             spectra = buffer.readlines()[1].split(",")
             spectra[1] = temp
             file.write(",".join([str(x) for x in spectra]))
@@ -18,17 +18,18 @@ def blend_in(buffer_path, temp):
 
 def main(logger):
     # NOTE: julabo i.p. address - 192.168.0.101
-    buffer_path, opc_ip = ".buffer.csv", "192.168.0.118"
+    buffer_path, data_path, opc_ip = ".buffer.csv", ".data/", "192.168.0.118"
     # logging.info(asyncio.run(temp_read(logger, opc_ip)))
-    # temp = "100.00"
+    temp = "100.00"
+    writer = GentleFileWriter(data_path, buffer_path)
 
     for _ in range(BROJ_MJERENJA):
         # NOTE: call demo.py
-        asyncio.run(wrapper(logger, buffer_path))
+        # asyncio.run(wrapper(logger, buffer_path))
         # NOTE: read external temperature from opc server
-        temp = asyncio.run(temp_read(logger, opc_ip))
+        # temp = asyncio.run(temp_read(logger, opc_ip))
         # NOTE: buffer + temp => file
-        blend_in(buffer_path, temp)
+        blend_in(writer, temp)
         time.sleep(VREMENSKI_ODMAK)
 
 
