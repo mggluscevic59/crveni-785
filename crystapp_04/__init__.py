ac6e365e0573a23f3d033304fd53fe058e68ce1a
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2022-11-24 11:47:45 +0100

    Running example
    Should work now. Check with laser if ok.

--- crystapp_04/__init__.py
+++ crystapp_04/__init__.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
 from .utils import set_filename, path_exists
-from .devices import temp_read, wrapper, class_wrapper, \
-    BROJ_MJERENJA, VREMENSKI_ODMAK
+from .devices import temp_read
 from .filewriter import GentleFileWriter
--- main.py
+++ main.py
@@ -7,8 +7,13 @@ import sys
 
 
 from demo_custom import WasatchDemo
-from crystapp_04 import wrapper, temp_read, \
-    GentleFileWriter, BROJ_MJERENJA, VREMENSKI_ODMAK
+from crystapp_04 import temp_read, GentleFileWriter
+
+
+INTEGRACIJSKO_VRIJEME = 500 # milisecond
+BROJ_OCITANJA_ZA_INTERPOLACIJU = 1
+BROJ_MJERENJA = 3
+VREMENSKI_ODMAK = 60000 # milisecond
 
 
 def blend_in(writer, temp):
@@ -29,14 +34,24 @@ def calc_wait(start:datetime, stop:datetime, delay):
     return supstracted
 
 
-def main(logger):
+def main():
     # NOTE: julabo i.p. address - 192.168.0.101
     buffer_path, data_path, opc_ip = ".buffer.csv", ".data/", "192.168.0.118"
     # logging.info(asyncio.run(temp_read(logger, opc_ip)))
     # temp = "100.00"
     writer = GentleFileWriter(data_path, buffer_path)
     # Raman instance
-    raman = None
+    mock_argv = [
+        "--outfile="+str(buffer_path),
+        "--max=1",
+        "--delay-ms=0",
+        "--integration-time-ms="+str(INTEGRACIJSKO_VRIJEME),
+        "--ascii-art",
+        "--scans-to-average="+str(BROJ_OCITANJA_ZA_INTERPOLACIJU),
+        "--log-leve="+logging.getLevelName(logging.DEBUG),
+    ]
+    # raman = class_wrapper(logger, buffer_path)
+    raman = WasatchDemo(mock_argv)
 
     if not raman.connect():
         return
@@ -47,7 +62,7 @@ def main(logger):
         raman.run()
         # asyncio.run(wrapper(logger, buffer_path))
         # NOTE: read external temperature from opc server
-        temp = asyncio.run(temp_read(logger, opc_ip))
+        temp = asyncio.run(temp_read(opc_ip))
         # NOTE: buffer + temp => file
         blend_in(writer, temp)
         end_time = datetime.datetime.now()
@@ -56,9 +71,7 @@ def main(logger):
 
 
 if __name__ == "__main__":
-    # LOG_LEVEL, MESSAGE_FORMAT = logging.INFO, \
-    #     '%(asctime)s [0x%(thread)08x] %(name)s %(levelname)-8s %(message)s'
-    LOG_LEVEL, MESSAGE_FORMAT = logging.DEBUG, \
-        '%(asctime)s [0x%(thread)08x] %(name)s %(levelname)-8s %(message)s'
-    logging.basicConfig(level=LOG_LEVEL, format=MESSAGE_FORMAT)
-    main(LOG_LEVEL)
+    FORMAT = '%(asctime)s [0x%(thread)08x] %(name)s %(levelname)-8s %(message)s'
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+    logging.getLogger("asyncua.client").setLevel(logging.WARNING)
+    main()
