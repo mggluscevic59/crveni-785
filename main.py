bcf7985ed1eef258e17f8d2e5f5964244361fef7
Mišo Grgur Gluščević <mggluscevic59@gmail.com>
2022-12-01 13:05:53 +0100

    Added process priority
    Gave this python program top priority across different Operating systems

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import asyncio
 # import time
 import datetime
 import platform
-
+import os
+import psutil
+import sys
 
 from crystapp_04 import wrapper, temp_read, \
     GentleFileWriter, BROJ_MJERENJA, VREMENSKI_ODMAK
@@ -30,12 +32,12 @@ def blend_in(writer: GentleFileWriter, temp):
 def calc_wait(start:datetime.datetime, stop:datetime.datetime, delay):
     passed = int((stop - start).microseconds*0.001)
     # logging.debug("%4d miliseconds", passed)
-    logging.debug("{0:4d} miliseconds".format(passed))
+    logging.debug("{0:5d} miliseconds".format(passed))
     if passed > delay:
         return 0
     supstracted = delay - passed
     # logging.info("%s miliseconds async sleep", supstracted)
-    logging.info("{0} miliseconds async sleep".format(supstracted))
+    logging.info(f"{supstracted} miliseconds async sleep")
     return supstracted*0.001
 
 
@@ -44,22 +46,32 @@ async def main(log_level):
 
     for i in range(BROJ_MJERENJA):
         start_time = datetime.datetime.now()
+        logging.debug("started measurement")
         task1 = asyncio.create_task(temp_read(log_level, OPC_REAL))
         # task1 = asyncio.create_task(temp_read(log_level, OPC_TEST))
         await wrapper(log_level, writer.buffer_path)
         blend_in(writer, await task1)
         end_time = datetime.datetime.now()
+        logging.debug("ended measurement")
 
-        logging.debug(i, BROJ_MJERENJA)
+        logging.debug([i, BROJ_MJERENJA])
         if i != (BROJ_MJERENJA - 1):
             await asyncio.sleep(calc_wait(start_time, end_time, VREMENSKI_ODMAK))
 
 
 if __name__ == "__main__":
+    os_used = sys.platform
+    process = psutil.Process(os.getpid())  # Set highest priority for the python script for the CPU
+    if os_used == "win32":  # Windows (either 32-bit or 64-bit)
+        process.nice(psutil.REALTIME_PRIORITY_CLASS)
+    elif os_used == "linux":  # linux
+        process.nice(psutil.IOPRIO_HIGH)
+    else:  # MAC OS X or other
+        process.nice(20) 
     FORMAT = '%(asctime)s [0x%(thread)08x] %(name)s %(levelname)-8s %(message)s'
     # log level
-    # logging.basicConfig(level=logging.DEBUG, format=FORMAT)
-    logging.basicConfig(level=logging.INFO, format=FORMAT)
+    logging.basicConfig(level=logging.DEBUG, format=FORMAT)
+    # logging.basicConfig(level=logging.INFO, format=FORMAT)
     logging.getLogger("asyncua.client").setLevel(logging.WARNING)
     # LOG_NAME = "wasatch.FeatureIdentificationDevice"
     # logging.getLogger(LOG_NAME).setLevel(logging.CRITICAL + 1)
